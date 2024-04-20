03f1b46f5069632c26ae427107e4ef852b89729b
pkphukhao <pk.weproject@gmail.com>
2024-04-20 13:46:03 +0700

    first commit

---
+++ src/Report.jsx
@@ -0,0 +1,108 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import { Row, Col, Card, Table } from "react-bootstrap";
+import axios from "axios";
+import Common from "./common";
+const BASE_URL = Common.API_URL;
+const options = Common.options;
+export default class report extends Component {
+  state = {
+    zipcode: 40000,
+    amphur_code: 0,
+    amphur_name: "",
+    province_code: 0,
+    province_name: "",
+    district: [],
+  };
+  getData = async () => {
+    try {
+      await axios
+        .get(`${BASE_URL}/${this.state.zipcode}`, options)
+        .then((response) => {
+          let res = response.data;
+
+          if (res.district === undefined) {
+            this.setState({
+              district: [],
+            });
+          }
+          this.setState({
+            amphur_name: res.amphur_name,
+            province_name: res.province_name,
+            district: res.district,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  filter = (e) => {
+    this.setState({
+      zipcode: e.target.value,
+    });
+    this.getData();
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+  render() {
+    const { district } = this.state;
+    return (
+      <div>
+        <Row>
+          <Col lg="9">
+            <div align="left">
+              <h3>
+                อำเภอ <u>{this.state.amphur_name}</u> จังหวัด{" "}
+                <u>
+                  {this.state.province_name} {this.state.zipcode}
+                </u>
+              </h3>
+            </div>
+          </Col>
+          <Col lg="3">
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder="ระบุเลขไปรษณีย์ 5 หลัก"
+                onChange={this.filter}
+                onKeyUp={this.filter}
+                maxLength="5"
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <div style={{ paddingTop: "15px" }}>
+          <Card>
+            <Card.Body>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>รหัสตำบล</th>
+                    <th>ตำบล</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {district?.map((rs, index) => (
+                    <tr key={index}>
+                      <td align="center">{index + 1}</td>
+                      <td>{rs.district_code}</td>
+                      <td>{rs.district_name}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
